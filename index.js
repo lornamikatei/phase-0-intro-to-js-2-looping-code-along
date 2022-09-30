8dd8066aa7cfbca7e2c5f42c541f3864bf036180
Mringa School <lorna.mikatei@student.moringaschool.com>
2022-09-30 10:08:37 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+//cons names = ["Guadalupe", "Ollie", "Aki"];
+const cards = ["Lisa", "Kaitlyn", "Jan", "surprise"];
+
+const newArray = [];
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        
+        newArray.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+return newArray
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
+countDown(4);
